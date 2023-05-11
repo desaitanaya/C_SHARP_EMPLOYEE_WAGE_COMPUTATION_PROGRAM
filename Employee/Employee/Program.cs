a6559fcd57f0bf667343b922b020e0bc4becea4e
Tanaya <desaitanaya17@gmail.com>
2023-05-11 18:11:00 +0530

    [Tanaya] Add : C# Program to check the attendance and Calculate the Wage for Part time Employee.

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -8,19 +8,29 @@
             Console.WriteLine("--------------------------------------------");
 
             Random random = new Random();
-            int attendance = random.Next(0, 2);
+            int attendance = random.Next(0, 3);
 
             int salary = 0;
 
             //Constant variables
+            const byte IS_PRESENT_FULL_TIME = 1;
+            const byte IS_PRESENT_PART_TIME = 2;
             const byte FULL_DAY_HOUR = 8;
+            const byte PART_TIME_HOUR = 4;
             const byte WAGE_PER_HOUR = 20;
 
             //Checking if the Employee is Present or not
-            if (attendance == 1)
+            if (attendance == IS_PRESENT_FULL_TIME)
             {
+                //Calculating the salary for Full time Employee
                 salary = FULL_DAY_HOUR * WAGE_PER_HOUR;
                 Console.WriteLine("Employee is Present for Full time and the Salary is Rs." + salary);
+            }
+            else if (attendance == IS_PRESENT_PART_TIME)
+            {
+                //Calculating the salary for Part time Employee
+                salary = PART_TIME_HOUR * WAGE_PER_HOUR;
+                Console.WriteLine("Employee is Present for Part time and the Salary is Rs." + salary);
             }
             else
             {
