beb6f13fcbb826b91e04d054032f84f51f232c64
Tanaya <desaitanaya17@gmail.com>
2023-05-11 18:02:57 +0530

    [Tanaya] Add : C# Program to check the attendance and Calculate the Daily Wage of an Employee.

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -10,14 +10,21 @@
             Random random = new Random();
             int attendance = random.Next(0, 2);
 
+            int salary = 0;
+
+            //Constant variables
+            const byte FULL_DAY_HOUR = 8;
+            const byte WAGE_PER_HOUR = 20;
+
             //Checking if the Employee is Present or not
             if (attendance == 1)
             {
-                Console.WriteLine("Employee is Present");
+                salary = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                Console.WriteLine("Employee is Present for Full time and the Salary is Rs." + salary);
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee is Absent and the Salary is Rs." + salary);
             }
         }
     }
