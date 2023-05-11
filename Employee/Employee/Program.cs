c836f8304e7e91027cb13701a38fef2f9b3f4e5e
Tanaya <desaitanaya17@gmail.com>
2023-05-11 19:35:50 +0530

    [Tanaya] Add : C# Program to Calculate Monthly wage of Employee till the Maximum Working day or Max Working Hour Condition is reached.

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -9,13 +9,14 @@
 
             Random random = new Random();
 
-            int salary = 0, daily_hour = 0, total_working_hour = 0, monthly_salary = 0 ;
+            int salary = 0, daily_hour = 0, total_working_hour = 0, monthly_salary = 0 , day =1;
 
             //Constant variables
             const byte IS_PRESENT_FULL_TIME = 1;
             const byte IS_PRESENT_PART_TIME = 2;
             const byte WAGE_PER_HOUR = 20;
             const byte TOTAL_WORKING_DAYS = 20;
+            const byte MAX_WORKING_HOURS = 100;
 
             /*
              * const byte FULL_DAY_HOUR = 8;
@@ -39,7 +40,8 @@
             }
             */
 
-            for(int day=1; day<=TOTAL_WORKING_DAYS; day++)
+            //for(int day=1; day<=TOTAL_WORKING_DAYS; day++)
+            while(day <= TOTAL_WORKING_DAYS && total_working_hour <= MAX_WORKING_HOURS)
             {
                 int attendance = random.Next(0, 3);
                 switch (attendance)
@@ -69,6 +71,7 @@
                 Console.WriteLine("Total working hours : " + total_working_hour);
                 Console.WriteLine("--------------------------------------------");
 
+                day++;
             }
 
             //Calculating Monthly salary of Employee
