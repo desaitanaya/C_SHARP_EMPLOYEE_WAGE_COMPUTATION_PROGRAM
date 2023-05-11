d2542aa5fc57faf15edadd77bb638a37db7a7781
Tanaya <desaitanaya17@gmail.com>
2023-05-11 19:28:23 +0530

    [Tanaya] Add : C# Program to Calculate Monthly wage of an Employee.

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -8,14 +8,14 @@
             Console.WriteLine("--------------------------------------------");
 
             Random random = new Random();
-            int attendance = random.Next(0, 3);
 
-            int salary = 0, daily_hour = 0;
+            int salary = 0, daily_hour = 0, total_working_hour = 0, monthly_salary = 0 ;
 
             //Constant variables
             const byte IS_PRESENT_FULL_TIME = 1;
             const byte IS_PRESENT_PART_TIME = 2;
             const byte WAGE_PER_HOUR = 20;
+            const byte TOTAL_WORKING_DAYS = 20;
 
             /*
              * const byte FULL_DAY_HOUR = 8;
@@ -39,24 +39,42 @@
             }
             */
 
-            switch (attendance)
+            for(int day=1; day<=TOTAL_WORKING_DAYS; day++)
             {
-                case IS_PRESENT_FULL_TIME:
-                    daily_hour = 8;
-                    Console.WriteLine("Employee is Present for Full time");
-                    break;
+                int attendance = random.Next(0, 3);
+                switch (attendance)
+                {
+                    case IS_PRESENT_FULL_TIME:
+                        daily_hour = 8;
+                        Console.WriteLine("Employee is Present for Full time");
+                        break;
 
-                case IS_PRESENT_PART_TIME:
-                    daily_hour = 4;
-                    Console.WriteLine("Employee is Present for Part time");
-                    break;
+                    case IS_PRESENT_PART_TIME:
+                        daily_hour = 4;
+                        Console.WriteLine("Employee is Present for Part time");
+                        break;
 
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                    default:
+                        daily_hour = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+
+                //Calculating salary of Employee
+                salary = daily_hour * WAGE_PER_HOUR;
+                Console.WriteLine("Salary of Employee is Rs." + salary);
+
+                //Calculating total working hour of Employee
+                total_working_hour = total_working_hour + daily_hour;
+                Console.WriteLine("Total working hours : " + total_working_hour);
+                Console.WriteLine("--------------------------------------------");
+
             }
-            salary = daily_hour * WAGE_PER_HOUR;
-            Console.WriteLine("Salary of Employee is Rs." + salary);
+
+            //Calculating Monthly salary of Employee
+            monthly_salary = total_working_hour * WAGE_PER_HOUR;
+            Console.WriteLine("Monthly salary of Employee is Rs." + monthly_salary);
+
         }
     }
 }
