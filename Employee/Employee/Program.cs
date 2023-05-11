5632154edd6c38f3779731d67be627343f826604
Tanaya <desaitanaya17@gmail.com>
2023-05-11 17:47:57 +0530

    [Tanaya] Add : C# Program to check the attendance of the Employee.

---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+            Console.WriteLine("--------------------------------------------");
+
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+
+            //Checking if the Employee is Present or not
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
