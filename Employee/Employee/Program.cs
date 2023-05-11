df82a732f0aecdf80f9feb4944451ff8af4611f9
Tanaya <desaitanaya17@gmail.com>
2023-05-11 18:21:58 +0530

    [Tanaya] Add : C# Program to check the attendance and CalculateDaily Wage of Employee using switch case.

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -10,15 +10,16 @@
             Random random = new Random();
             int attendance = random.Next(0, 3);
 
-            int salary = 0;
+            int salary = 0, daily_hour = 0;
 
             //Constant variables
             const byte IS_PRESENT_FULL_TIME = 1;
             const byte IS_PRESENT_PART_TIME = 2;
-            const byte FULL_DAY_HOUR = 8;
-            const byte PART_TIME_HOUR = 4;
             const byte WAGE_PER_HOUR = 20;
 
+            /*
+             * const byte FULL_DAY_HOUR = 8;
+               const byte PART_TIME_HOUR = 4;
             //Checking if the Employee is Present or not
             if (attendance == IS_PRESENT_FULL_TIME)
             {
@@ -36,6 +37,26 @@
             {
                 Console.WriteLine("Employee is Absent and the Salary is Rs." + salary);
             }
+            */
+
+            switch (attendance)
+            {
+                case IS_PRESENT_FULL_TIME:
+                    daily_hour = 8;
+                    Console.WriteLine("Employee is Present for Full time");
+                    break;
+
+                case IS_PRESENT_PART_TIME:
+                    daily_hour = 4;
+                    Console.WriteLine("Employee is Present for Part time");
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+            salary = daily_hour * WAGE_PER_HOUR;
+            Console.WriteLine("Salary of Employee is Rs." + salary);
         }
     }
 }
