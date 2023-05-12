76a5428592e1d03769cbf3b2a81b34e1be9149f2
Tanaya <desaitanaya17@gmail.com>
2023-05-12 16:30:37 +0530

    [Tanaya] Add : C# Program to calculate the Employee Monthly Wage using object oriented approach.

---
+++ Employee/Employee/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Employee
+{
+    internal class Employee
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+            Console.WriteLine("--------------------------------------------");
+
+            EmployeeWage.MonthlyWage();
+        }
+    }
+}
+        
